a0187e4eab6a00a128771731798b33a1055d6c23
et508 <ect508@gmail.com>
2025-05-23 00:25:51 -0400

    u1

--- Plugin.cs
+++ Plugin.cs
@@ -13,7 +13,7 @@ namespace LootManager
         private void Awake()
         {
             Log = Logger;
-            Log.LogInfo("[LootManager] Plugin loaded.");
+            Log.LogInfo("LootManager loaded.");
             var harmony = new Harmony("et508.erenshor.lootmanager");
             harmony.PatchAll();
         }
@@ -22,17 +22,41 @@ namespace LootManager
     [HarmonyPatch(typeof(LootWindow), nameof(LootWindow.LootAll))]
     public static class LootWindowLogPatch
     {
-        public static void Prefix(LootWindow __instance)
+        public static bool Prefix(LootWindow __instance)
         {
             foreach (ItemIcon itemIcon in __instance.LootSlots)
             {
                 if (!(itemIcon.MyItem == GameData.PlayerInv.Empty))
                 {
-                    UpdateSocialLog.LogAdd("[LootManager] Looted " + itemIcon.MyItem.ItemName, "lightblue");
+                    bool flag;
+                    if (itemIcon.MyItem.RequiredSlot == Item.SlotType.General)
+                    {
+                        flag = GameData.PlayerInv.AddItemToInv(itemIcon.MyItem);
+                    }
+                    else
+                    {
+                        flag = GameData.PlayerInv.AddItemToInv(itemIcon.MyItem, itemIcon.Quantity);
+                        int quantity = itemIcon.Quantity;
+                        int quantity2 = itemIcon.Quantity;
+                        int quantity3 = itemIcon.Quantity;
+                    }
+                    if (flag)
+                    {
+                        UpdateSocialLog.LogAdd("[Loot Manager] Looted Item: " + itemIcon.MyItem.ItemName, "yellow");
+                        itemIcon.InformGroupOfLoot(itemIcon.MyItem);
+                        itemIcon.MyItem = GameData.PlayerInv.Empty;
+                        itemIcon.UpdateSlotImage();
+                    }
+                    else
+                    {
+                        UpdateSocialLog.LogAdd("No room for " + itemIcon.MyItem.ItemName, "yellow");
+                    }
                 }
             }
             UpdateSocialLog.LogAdd("[LootManager] LootAll was called.", "yellow");
             Plugin.Log.LogInfo("[LootManager] LootAll was called.");
+
+            return false;
         }
     }
 }
