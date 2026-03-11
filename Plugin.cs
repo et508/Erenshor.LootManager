69fbb36324a5917d8afd269b51099d314f26a94d
et508 <ect508@gmail.com>
2026-03-11 16:31:19 -0400

    feat: update plugin version to 3.0.0 in BepInPlugin attribute

--- Plugin.cs
+++ Plugin.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 namespace LootManager
 {
-    [BepInPlugin("et508.erenshor.lootmanager", "Loot Manager", "2.2.0")]
+    [BepInPlugin("et508.erenshor.lootmanager", "Loot Manager", "3.0.0")]
     [BepInProcess("Erenshor.exe")]
     public class Plugin : BaseUnityPlugin
     {
